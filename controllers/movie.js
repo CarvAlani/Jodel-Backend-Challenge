668f1d6fcc0f1a226a93c1de069ee0718674fed9
Carlos Avilan <avilancj@gmail.com>
2018-01-30 16:36:43 -0400

    Use cache and flush it when new records are created

--- controllers/movie.js
+++ controllers/movie.js
@@ -1,23 +1,28 @@
 const httpStatus = require('http-status');
 const Movie = require('../models/movie');
 const APIError = require('../helpers/APIError');
+const { client, isCached } = require('../helpers/redis');
+const { getResults } = require('../helpers/mongo');
+const utils = require('../helpers/utils');
+const Promise = require('bluebird');
 
 const MovieController = {
   readAll(req, res, next) {
     const filter = req.query.filter || {};
+    const filterArray = utils.objectToKeyValueString(req.query.filter);
     const limit = req.query.limit !== undefined ? parseInt(req.query.limit, 10) : 10;
     const page = req.query.page !== undefined ? parseInt(req.query.page, 10) : 1;
     const offset = (page - 1) * limit;
-    // TODO: Add redis cache
-    Movie.find(filter)
-      .skip(offset)
-      .limit(limit)
+    filterArray.push('movies');
+    let filtersName = filterArray.join(':');
+    filtersName = `${filtersName}:limit:${limit}:offset:${offset}`;
+    return isCached(filtersName, filterArray)
+      .then(cached => getResults(filter, filterArray, filtersName, limit, offset, cached))
       .then(movies => res.json({
         movies,
         page,
         count: movies.length,
         limit,
-        // totalPages ?
       }))
       .catch(next);
   },
@@ -29,6 +34,7 @@ const MovieController = {
         if (!movie) {
           return Movie.create(req.body)
             .then(newMovie => res.json(newMovie))
+            .then(() => client.flushdb) // remove only specific smembers from new object
             .catch(next);
         }
         return Promise.reject(new APIError(
